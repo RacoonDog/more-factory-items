3f4033a7b9015224709c1033bec08be9a4774156
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2020-07-22 16:33:55 -0400

    Update RegistrationHandler.java

--- src/main/java/io/github/racoondog/more_factory_items/RegistrationHandler.java
+++ src/main/java/io/github/racoondog/more_factory_items/RegistrationHandler.java
@@ -12,11 +12,11 @@ public class RegistrationHandler {
 	@SubscribeEvent
 	public static void registerItems(Register<Item> event) {
 		final Item[] items = {
+				//Rods/Gearboxes
 				RegistryUtil.setItemName(new Item(), "aluminum_gearbox", "gearboxAluminum").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "aluminum_rod", "rodAluminum").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "bronze_gearbox", "gearboxBronze").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "bronze_rod", "rodBronze").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
-				RegistryUtil.setItemName(new Item(), "complicated_factory_blend", "factoryblendComplicated").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "constantan_gearbox", "gearboxConstantan").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "constantan_rod", "rodConstantan").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "copper_gearbox", "gearboxCopper").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
@@ -39,18 +39,23 @@ public class RegistrationHandler {
 				RegistryUtil.setItemName(new Item(), "nickel_rod", "rodNickel").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "platinum_gearbox", "gearboxPlatinum").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "platinum_rod", "rodPlatinum").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
-				RegistryUtil.setItemName(new Item(), "rare_factory_blend", "factoryblendRare").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
-				RegistryUtil.setItemName(new Item(), "resonant_factory_blend", "factoryblendResonant").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "signalum_gearbox", "gearboxSignalum").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "signalum_rod", "rodSignalum").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "silver_gearbox", "gearboxSilver").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "silver_rod", "rodSilver").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
-				RegistryUtil.setItemName(new Item(), "simple_factory_blend", "factoryblendSimple").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "steel_gearbox", "gearboxSteel").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "steel_rod", "rodSteel").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "tin_gearbox", "gearboxTin").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
 				RegistryUtil.setItemName(new Item(), "tin_rod", "rodTin").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
+				
+				//Vanilla Rods/Gearboxes
 				RegistryUtil.setItemName(new Item(), "wooden_gearbox", "gearboxWood").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
+				
+				//Factory Blends
+				RegistryUtil.setItemName(new Item(), "simple_factory_blend", "factoryblendSimple").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
+				RegistryUtil.setItemName(new Item(), "complicated_factory_blend", "factoryblendComplicated").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
+				RegistryUtil.setItemName(new Item(), "rare_factory_blend", "factoryblendRare").setCreativeTab(MoreFactoryItems.CREATIVE_TAB),
+				RegistryUtil.setItemName(new Item(), "resonant_factory_blend", "factoryblendResonant").setCreativeTab(MoreFactoryItems.CREATIVE_TAB)
 		};
 
 		event.getRegistry().registerAll(items);
