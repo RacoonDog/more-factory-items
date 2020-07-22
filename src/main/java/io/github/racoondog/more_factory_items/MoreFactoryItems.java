d458f629296438409356a08bbc15736927dbf22f
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2020-07-22 16:30:32 -0400

    Update MoreFactoryItems.java

--- src/main/java/io/github/racoondog/more_factory_items/MoreFactoryItems.java
+++ src/main/java/io/github/racoondog/more_factory_items/MoreFactoryItems.java
@@ -19,7 +19,7 @@ public class MoreFactoryItems {
 
 	public static final String MODID = "morefactoryitems";
 	public static final String NAME = "More Factory Items";
-	public static final String VERSION = "2.0.0";
+	public static final String VERSION = "2.2.0";
 	public static final String MC_VERSION = "[1.12.2]";
 
 	public static final Logger LOGGER = LogManager.getLogger(MoreFactoryItems.MODID);
@@ -32,45 +32,96 @@ public class MoreFactoryItems {
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
 		LOGGER.info("Init Phase");
-		RegistryUtil.OreDict(ModItems.aluminum_rod, "rodAluminum");
+		// --Registering Oredict Entries--
+		//Rods/Gearboxes
 		RegistryUtil.OreDict(ModItems.aluminum_gearbox, "gearboxAluminum");
-		RegistryUtil.OreDict(ModItems.bronze_rod, "rodBronze");
+		RegistryUtil.OreDict(ModItems.aluminum_rod, "rodAluminum");
 		RegistryUtil.OreDict(ModItems.bronze_gearbox, "gearboxBronze");
-		RegistryUtil.OreDict(ModItems.complicated_factory_blend, "factoryblendComplicated");
-		RegistryUtil.OreDict(ModItems.constantan_rod, "rodConstantan");
+		RegistryUtil.OreDict(ModItems.bronze_rod, "rodBronze");
 		RegistryUtil.OreDict(ModItems.constantan_gearbox, "gearboxConstantan");
-		RegistryUtil.OreDict(ModItems.copper_rod, "rodCopper");
+		RegistryUtil.OreDict(ModItems.constantan_rod, "rodConstantan");
 		RegistryUtil.OreDict(ModItems.copper_gearbox, "gearboxCopper");
-		RegistryUtil.OreDict(ModItems.electrum_rod, "rodElectrum");
+		RegistryUtil.OreDict(ModItems.copper_rod, "rodCopper");
 		RegistryUtil.OreDict(ModItems.electrum_gearbox, "gearboxElectrum");
-		RegistryUtil.OreDict(ModItems.enderium_rod, "rodEnderium");
+		RegistryUtil.OreDict(ModItems.electrum_rod, "rodElectrum");
 		RegistryUtil.OreDict(ModItems.enderium_gearbox, "gearboxEnderium");
-		RegistryUtil.OreDict(ModItems.invar_rod, "rodInvar");
+		RegistryUtil.OreDict(ModItems.enderium_rod, "rodEnderium");
 		RegistryUtil.OreDict(ModItems.invar_gearbox, "gearboxInvar");
-		RegistryUtil.OreDict(ModItems.iridium_rod, "rodIridium");
+		RegistryUtil.OreDict(ModItems.invar_rod, "rodInvar");
 		RegistryUtil.OreDict(ModItems.iridium_gearbox, "gearboxIridium");
-		RegistryUtil.OreDict(ModItems.lead_rod, "rodLead");
+		RegistryUtil.OreDict(ModItems.iridium_rod, "rodIridium");
 		RegistryUtil.OreDict(ModItems.lead_gearbox, "gearboxLead");
-		RegistryUtil.OreDict(ModItems.lumium_rod, "rodLumium");
+		RegistryUtil.OreDict(ModItems.lead_rod, "rodLead");
 		RegistryUtil.OreDict(ModItems.lumium_gearbox, "gearboxLumium");
-		RegistryUtil.OreDict(ModItems.mithril_rod, "rodMithril");
+		RegistryUtil.OreDict(ModItems.lumium_rod, "rodLumium");
 		RegistryUtil.OreDict(ModItems.mithril_gearbox, "gearboxMithril");
-		RegistryUtil.OreDict(ModItems.nickel_rod, "rodNickel");
+		RegistryUtil.OreDict(ModItems.mithril_rod, "rodMithril");
 		RegistryUtil.OreDict(ModItems.nickel_gearbox, "gearboxNickel");
-		RegistryUtil.OreDict(ModItems.platinum_rod, "rodPlatinum");
+		RegistryUtil.OreDict(ModItems.nickel_rod, "rodNickel");
 		RegistryUtil.OreDict(ModItems.platinum_gearbox, "gearboxPlatinum");
+		RegistryUtil.OreDict(ModItems.platinum_rod, "rodPlatinum");
+		RegistryUtil.OreDict(ModItems.signalum_gearbox, "gearboxSignalum");
+		RegistryUtil.OreDict(ModItems.signalum_rod, "rodSignalum");
+		RegistryUtil.OreDict(ModItems.silver_gearbox, "gearboxSilver");
+		RegistryUtil.OreDict(ModItems.silver_rod, "rodSilver");
+		RegistryUtil.OreDict(ModItems.steel_gearbox, "gearboxSteel");
+		RegistryUtil.OreDict(ModItems.steel_rod, "rodSteel");
+		RegistryUtil.OreDict(ModItems.tin_gearbox, "gearboxTin");
+		RegistryUtil.OreDict(ModItems.tin_rod, "rodTin");
+		
+		//Vanilla Rods/Gearboxes
+		RegistryUtil.OreDict(ModItems.wooden_gearbox, "gearboxWood");
+		
+		//Factory Blends
+		RegistryUtil.OreDict(ModItems.simple_factory_blend, "factoryblendSimple");
+		RegistryUtil.OreDict(ModItems.complicated_factory_blend, "factoryblendComplicated");
 		RegistryUtil.OreDict(ModItems.rare_factory_blend, "factoryblendRare");
 		RegistryUtil.OreDict(ModItems.resonant_factory_blend, "factoryblendResonant");
-		RegistryUtil.OreDict(ModItems.signalum_rod, "rodSignalum");
-		RegistryUtil.OreDict(ModItems.signalum_gearbox, "gearboxSignalum");
-		RegistryUtil.OreDict(ModItems.silver_rod, "rodSilver");
-		RegistryUtil.OreDict(ModItems.silver_gearbox, "gearboxSilver");
-		RegistryUtil.OreDict(ModItems.simple_factory_blend, "factoryblendSimple");
-		RegistryUtil.OreDict(ModItems.steel_rod, "rodSteel");
-		RegistryUtil.OreDict(ModItems.steel_gearbox, "gearboxSteel");
-		RegistryUtil.OreDict(ModItems.tin_rod, "rodTin");
-		RegistryUtil.OreDict(ModItems.tin_gearbox, "gearboxTin");
-		RegistryUtil.OreDict(ModItems.wooden_gearbox, "gearboxWood");
+		
+		// -Other Entries-
+		//Metal Rods
+		RegistryUtil.OreDict(ModItems.aluminum_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.bronze_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.constantan_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.copper_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.electrum_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.enderium_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.invar_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.iridium_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.lead_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.lumium_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.mithril_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.nickel_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.platinum_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.signalum_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.silver_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.steel_rod, "rodMetalAny");
+		RegistryUtil.OreDict(ModItems.tin_rod, "rodMetalAny");
+		
+		//Metal Gearboxes
+		RegistryUtil.OreDict(ModItems.aluminum_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.bronze_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.constantan_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.copper_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.electrum_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.enderium_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.invar_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.iridium_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.lead_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.lumium_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.mithril_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.nickel_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.platinum_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.signalum_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.silver_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.steel_gearbox, "gearboxMetalAny");
+		RegistryUtil.OreDict(ModItems.tin_gearbox, "gearboxMetalAny");
+		
+		//Factory Blends
+		RegistryUtil.OreDict(ModItems.simple_factory_blend, "factoryblendAny");
+		RegistryUtil.OreDict(ModItems.complicated_factory_blend, "factoryblendAny");
+		RegistryUtil.OreDict(ModItems.rare_factory_blend, "factoryblendAny");
+		RegistryUtil.OreDict(ModItems.resonant_factory_blend, "factoryblendAny");
 	}
 
 	@EventHandler
