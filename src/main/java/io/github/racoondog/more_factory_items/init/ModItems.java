eb58d6ee7843340627e314a7dfdb68e83c5ec2fa
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2020-07-22 15:27:07 -0400

    Create ModItems.java

---
+++ src/main/java/io/github/racoondog/more_factory_items/init/ModItems.java
@@ -0,0 +1,50 @@
+package io.github.racoondog.more_factory_items.init;
+
+import io.github.racoondog.more_factory_items.MoreFactoryItems;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+@ObjectHolder(MoreFactoryItems.MODID)
+public class ModItems {
+	
+	public static final Item aluminum_gearbox = null;
+	public static final Item aluminum_rod = null;
+	public static final Item bronze_gearbox = null;
+	public static final Item bronze_rod = null;
+	public static final Item complicated_factory_blend = null;
+	public static final Item constantan_gearbox = null;
+	public static final Item constantan_rod = null;
+	public static final Item copper_gearbox = null;
+	public static final Item copper_rod = null;
+	public static final Item electrum_gearbox = null;
+	public static final Item electrum_rod = null;
+	public static final Item enderium_gearbox = null;
+	public static final Item enderium_rod = null;
+	public static final Item invar_gearbox = null;
+	public static final Item invar_rod = null;
+	public static final Item iridium_gearbox = null;
+	public static final Item iridium_rod = null;
+	public static final Item lead_gearbox = null;
+	public static final Item lead_rod = null;
+	public static final Item lumium_gearbox = null;
+	public static final Item lumium_rod = null;
+	public static final Item mithril_gearbox = null;
+	public static final Item mithril_rod = null;
+	public static final Item nickel_gearbox = null;
+	public static final Item nickel_rod = null;
+	public static final Item platinum_gearbox = null;
+	public static final Item platinum_rod = null;
+	public static final Item rare_factory_blend = null;
+	public static final Item resonant_factory_blend = null;
+	public static final Item signalum_gearbox = null;
+	public static final Item signalum_rod = null;
+	public static final Item silver_gearbox = null;
+	public static final Item silver_rod = null;
+	public static final Item simple_factory_blend = null;
+	public static final Item steel_gearbox = null;
+	public static final Item steel_rod = null;
+	public static final Item tin_gearbox = null;
+	public static final Item tin_rod = null;
+	public static final Item wooden_gearbox = null;
+	
+}
